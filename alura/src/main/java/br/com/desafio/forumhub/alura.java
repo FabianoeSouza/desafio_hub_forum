8cb86527c092b0f4ad9c81b7aadce53327f1b377
FabianoeSouza <fabiano.lotar@yahoo.com.br>
2024-07-09 21:32:00 -0300

    Add files via upload

---
+++ alura/src/main/java/br/com/desafio/forumhub/alura.java
@@ -0,0 +1,13 @@
+package br.com.desafio.forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class alura {
+
+	public static void main(String[] args) {
+		SpringApplication.run(alura.class, args);
+	}
+
+}
